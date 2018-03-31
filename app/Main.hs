bc88f7ea8c90e73690d561e2d41456ae6dd83f1e
FH <morpheus.hu14@gmail.com>
2018-03-31 10:46:42 -0400

    better structure

--- app/Main.hs
+++ app/Main.hs
@@ -6,24 +6,27 @@ import Data.Aeson.Types
 import Network.HTTP.Simple
 import Network.HTTP.Client.TLS
 import System.Environment
+import TimeStamped
 import qualified Data.Vector as V
 
-dataUrl :: IO (Maybe String)
+dataUrl :: IO (Maybe (TimeStamped String))
 dataUrl = do
   apiKey <- getEnv "BLZ_API_KEY"
   res <- fetchJSON ("https://us.api.battle.net/wow/auction/data/illidan?locale=en_US&apikey=" ++ apiKey)
-  return $ parseMaybe parseDataUrl res
+  return $ parseMaybe parseMetaData res
 
 fetchJSON :: String -> IO Value
 fetchJSON url = do
   req <- parseRequest $ "GET " ++ url
   fmap getResponseBody (httpJSON req)
 
-parseDataUrl :: Value -> Parser String
-parseDataUrl value = do
+parseMetaData :: Value -> Parser (TimeStamped String)
+parseMetaData value = do
   files <- withObject "Look up files" (.: "files") value
   firstObj <- withArray "Get first file" (pure . (V.! 0)) files
-  withObject "Lookup data URL" (.: "url") firstObj
+  lastModified <- withObject "Lookup last mofidifed time" (.: "lastModified") firstObj
+  url <- withObject "Lookup data URL" (.: "url") firstObj
+  pure (Stamp url lastModified)
 
 main :: IO ()
 main = do
@@ -31,4 +34,4 @@ main = do
   dataUrl <- dataUrl
   putStrLn $ case dataUrl of
     Nothing -> "Parsing failed!"
-    Just url -> "URL is: " ++ url
+    Just (Stamp url lastModified) -> "URL (lastModified=" ++ (show lastModified) ++ ") is: " ++ url
